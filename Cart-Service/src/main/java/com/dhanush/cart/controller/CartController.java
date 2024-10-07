90023a12fa77a810d6ad31ec6ab617bcd4755572
techieupgraders <techieupgraders@gmail.com>
2024-10-08 00:19:56 +0530

    redis setup completed

--- Cart-Service/src/main/java/com/dhanush/cart/controller/CartController.java
+++ Cart-Service/src/main/java/com/dhanush/cart/controller/CartController.java
@@ -3,33 +3,23 @@ package com.dhanush.cart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dhanush.cart.model.ItemLine;
 import com.dhanush.cart.repository.CarRepositoryImpl;
+import com.dhanush.cart.service.CartService;
 
 @RestController
 public class CartController {
-
+	
 	@Autowired
-	private CarRepositoryImpl cart;
+	private CartService cartService;
 
-	@Autowired
-	private pricefeign pf;
-
-	@PostMapping("/api/cart/dhanush")
-	public ItemLine saveCart(@RequestBody ItemLine itemline) {
-		System.out.println("inside");
-		Double feignprice = pf.getPriceById(itemline.getItem().getId());
-		itemline.getItem().setPrice(feignprice);
-		// System.out.println("price:" +itemline.getItem().getPrice());
-		// System.out.println("qty:" +itemline.getQty());
-		// if(itemline.getItem().getItemtotal()==null) {
-		double tot = itemline.getItem().getPrice() * itemline.getQty();
-		// System.out.println("tot:" +tot);
-		itemline.getItem().setItemtotal(tot);
-		ItemLine itl = new ItemLine(itemline.getItem(), itemline.getQty());
-		cart.save("dhanush", itl);
-		return itl;
+	@PostMapping("/api/cart/{user}")
+	public ItemLine saveCart(@RequestBody ItemLine itemline, @RequestParam String user) {
+		ItemLine saveItemLine = cartService.saveCart(itemline,user);
+		return saveItemLine;
+		
 	}
 }
--- Cart-Service/src/main/java/com/dhanush/cart/model/ItemLine.java
+++ Cart-Service/src/main/java/com/dhanush/cart/model/ItemLine.java
@@ -2,40 +2,21 @@ package com.dhanush.cart.model;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemLine implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Item item;
 
 	private int qty;
-	
-	public ItemLine() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public ItemLine(Item item, int qty) {
-		super();
-		this.item = item;
-		this.qty = qty;
-	}
-
-	public Item getItem() {
-		return item;
-	}
-
-	public void setItem(Item item) {
-		this.item = item;
-	}
-
-	public int getQty() {
-		return qty;
-	}
-
-	public void setQty(int qty) {
-		this.qty = qty;
-	}
-
-	
 
 }
